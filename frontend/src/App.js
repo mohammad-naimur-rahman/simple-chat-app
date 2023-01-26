da497349c1743f432f7d13b59444f53b54ba959f
Naimur Rahman <64221853+mohammad-naimur-rahman@users.noreply.github.com>
2023-01-26 11:22:01 +0600

    deleted extra component in route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='App'>
       <Switch>
-        <Route path='/' component={Homepage} exact>
+        <Route path='/' exact>
           <Homepage />
         </Route>
         <ProtectedRoute>
